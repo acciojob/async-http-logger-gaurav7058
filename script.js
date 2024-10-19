000b9cfbb0221ae8097bc755e0aa8c00cb8ff46f
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-10-19 19:37:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+const getApi=async()=>{
+	const response=await fetch("https://jsonplaceholder.typicode.com/todos/1");
+	const json=await response.json();
+	console.log(json)
+}
+getApi()
